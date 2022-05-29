bfd4202887aa74cd5765c16202dfc0a4a74e178a
yongsin0129 <yongsin0129@gmail.com>
2022-05-29 16:30:12 +0800

    feat : add config mongoose

--- routes/modules/finished.js
+++ routes/modules/finished.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('finished')
+  const input = req.query.input_URL
+  res.send(input)
 })
 
 module.exports = router
